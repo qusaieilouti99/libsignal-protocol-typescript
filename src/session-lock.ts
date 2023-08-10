3bf074de272bf732148a67bbbf69e02f6a714093
Qusai Eilouti <Qusai@zetaton.com>
2023-08-10 15:52:02 +0300

    queue

--- src/session-lock.ts
+++ src/session-lock.ts
@@ -3,62 +3,64 @@
  * jobQueue manages multiple queues indexed by device to serialize
  * session io ops on the database.
  */
-import PQueue from 'p-queue'
+
 /*const jobQueue: { [k: string]: Promise<any> } = {}
 
-export type JobType<T> = () => Promise<T>
+ export type JobType<T> = () => Promise<T>
 
-export class SessionLock {
-    static errors: any[] = []
-    static _promises: Promise<any>[] = []
-    static queueJobForNumber<T>(id: string, runJob: JobType<T>): Promise<T> {
-        const runPrevious = jobQueue[id] || Promise.resolve()
-        const runCurrent = (jobQueue[id] = runPrevious.then(runJob, runJob))
-        const promise = runCurrent
-            .then(function () {
-                if (jobQueue[id] === runCurrent) {
-                    delete jobQueue[id]
-                }
-            })
-            .catch((e) => {
-                // SessionLock callers should already have seen these errors on their own
-                // Promise chains, but we need to handle them here too so we just save them
-                // so callers can review them.
-                SessionLock.errors.push(e)
-            })
-        SessionLock._promises.push(promise)
-        return runCurrent
-    }
+ export class SessionLock {
+ static errors: any[] = []
+ static _promises: Promise<any>[] = []
+ static queueJobForNumber<T>(id: string, runJob: JobType<T>): Promise<T> {
+ const runPrevious = jobQueue[id] || Promise.resolve()
+ const runCurrent = (jobQueue[id] = runPrevious.then(runJob, runJob))
+ const promise = runCurrent
+ .then(function () {
+ if (jobQueue[id] === runCurrent) {
+ delete jobQueue[id]
+ }
+ })
+ .catch((e) => {
+ // SessionLock callers should already have seen these errors on their own
+ // Promise chains, but we need to handle them here too so we just save them
+ // so callers can review them.
+ SessionLock.errors.push(e)
+ })
+ SessionLock._promises.push(promise)
+ return runCurrent
+ }
 
-    static async clearQueue(): Promise<void> {
-        await Promise.all(SessionLock._promises)
-    }
-}*/
+ static async clearQueue(): Promise<void> {
+ await Promise.all(SessionLock._promises)
+ }
+ }*/
+import { queue, QueueObject } from 'async'
 
 type JobType<T> = () => Promise<T>
 
 export class SessionLock {
-    private static queues: { [id: string]: PQueue } = {}
+    private static queues: { [id: string]: QueueObject<any> } = {}
 
     static async queueJobForNumber<T>(id: string, runJob: JobType<T>): Promise<T> {
         if (!this.queues[id]) {
-            this.queues[id] = new PQueue({ concurrency: 1 })
+            this.queues[id] = queue<JobType<T>>(async (job, callback) => {
+                try {
+                    const result = await job() // Return the result from runJob
+                    callback()
+                    return result
+                } finally {
+                    console.log('finally')
+                    if (this.queues[id].length() === 0) {
+                        this.queues[id].kill()
+                        delete this.queues[id]
+                    }
+                }
+            })
         }
 
-        const queue = this.queues[id]
+        const myQueue = this.queues[id]
 
-        const result = await queue.add(async () => {
-            try {
-                return await runJob() // Return the result from runJob
-            } finally {
-                console.log('finally')
-                if (queue.size === 0) {
-                    delete this.queues[id]
-                }
-            }
-        })
-
-        return result as T // Return the result from the wrapped function
+        return await myQueue.push<T>(runJob) // Return the result from the wrapped function
     }
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
